bd115518f1ccb528d215ab3aedfd508cbdfc5bf5
NIVEDITA <nive2104@users.noreply.github.com>
2016-12-15 14:03:46 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,17 @@
+package com.example.android.anuragtpc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nivedita
+ on 25-08-2016.
+ */
+public class Contact extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact);
+
+    }
+}
